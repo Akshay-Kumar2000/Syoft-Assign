38605ad41443bb82d85230295d2b7010a9caa156
Akshay Kumar <93372936+Akshay-Kumar2000@users.noreply.github.com>
2022-08-02 20:21:19 +0530

    Add files via upload

---
+++ login-registration-frontend/src/components/admin.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Admin = () => {
+    return(
+        <div>
+            <h2>Let's build the admin page here</h2>
+        </div>
+    )
+}
---
+++ login-registration-frontend/src/components/userDetails.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class UserDetails extends Component {
+  
+  render() {
+    return (
+      <div>
+        <h2>Let's start working</h2>
+      </div>
+    );
+  }
+}
+
+
